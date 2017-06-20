3fe43f1511a35573e9fc93beba80064ec1e6e868
cemvarol <cem.varol@outlook.com>
2017-06-20 07:06:26 -0700

    Add files via upload

---
+++ LAB2B.ps1
@@ -0,0 +1,13 @@
+﻿######Ex1
+#Task1
+get-process
+get-process | sort name -Descending |select name,id,vm,pm
+help ConvertTo-Html -ShowWindow
+Get-Process |sort -Descending |ConvertTo-Html -Property name,id,vm,pm
+Get-Process |sort -Descending |ConvertTo-Html -Property name,id,vm,pm |Out-File ProcReport.html
+invoke-expression .\ProcReport.html
+get-process |sort name -Descending | ConvertTo-Html -Property name,id,pm,vm,pm -PreContent "Processes" -PostContent (get-date) |Out-File Proc-Report2.html
+invoke-expression .\Proc-Report2.html
+######Ex2
+Task1
+Get-EventLog –Newest 10 –LogName System Get-EventLog –Newest 10 –LogName System |  ConvertTo-CSV
---
+++ Lab2A.ps1
@@ -0,0 +1,33 @@
+﻿#Task1
+Get-Date
+get-date -dayofyear
+get-date |Get-Member
+get-date |Select-Object -Property dayofyear
+#Task2
+help *hotfix*
+get-hotfix | Get-Member
+Get-HotFix | Select-Object -Property hofixid,ınstalledon,installedby
+#Task3
+help *scope*
+help Get-DhcpServerv4Scope -ShowWindow
+get-dhcpserverv4scope -ComputerName lon-dc1 |Select-Object -Property scopeid,subnetmask,name
+#Task4
+help *rule*
+Get-NetFirewallRule
+help get-netfirewallrule -ShowWindow
+Get-NetFirewallRule -enabled True
+Get-NetFirewallRule -Enabled True |Select-Object -Property DisplayName,profile,direction,action |Sort-Object -Property -displayname
+#Task5
+help *neighbor*
+
+help Get-NetNeighbor -ShowWindow
+Get-NetNeighbor
+Get-NetNeighbor |Sort-Object -Property state
+Get-NetNeighbor |Sort-Object -Property State | Select-Object -Property IPAddress,State
+#Task6
+ping lon-dc1
+ping lon-cl1
+help *cache*
+Get-DnsClientCache
+Get-DnsClientCache |sort Name |select name,type,TimeToLive
+
